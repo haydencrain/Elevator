f12758cb7a38a15e699adc67099ef3d56df6bae0
Hayden Crain <hayden@canva.com>
2021-08-09 12:47:42 +1000

    Adds Elevator class

---
+++ src/com/dos/elevator/Elevator.java
@@ -0,0 +1,90 @@
+package com.dos.elevator;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Elevator {
+    enum ElevatorState {
+        MOVING_UP,
+        MOVING_DOWN,
+        SERVING,
+        IDLE,
+    }
+
+    private int floor;
+    private ElevatorState state;
+    private final ElevatorQueue stops;
+    private final HashMap<Integer, HashSet<Integer>> requestsMap;
+
+    public Elevator() {
+        floor = 0;
+        state = ElevatorState.IDLE;
+        stops = new ElevatorQueue();
+        requestsMap = new HashMap<>();
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public ElevatorState getState() {
+        return state;
+    }
+
+    public void move() {
+        var nextStop = stops.peek();
+        updateState(nextStop);
+        floor += getDirectionToMove();
+        if (floor == nextStop) {
+            // remove from stops
+            var stop = stops.poll();
+            // retrieve list of requests that was recorded for that stop
+            var requests = requestsMap.get(stop);
+            // add them to the queue
+            stops.add(requests);
+        }
+    }
+
+    public void addRequest(int source, int destination) {
+        stops.add(source);
+        addDestination(source, destination);
+    }
+
+    private void addDestination(int source, int destination) {
+        HashSet<Integer> requests;
+        if (!requestsMap.containsKey(source)) {
+            requests = new HashSet<Integer>(destination);
+        } else {
+            requests = requestsMap.get(source);
+            requests.add(destination);
+        }
+        requestsMap.put(source, requests);
+    }
+
+    private void updateState(Integer nextStop) {
+        if (nextStop == null) {
+            state = ElevatorState.IDLE;
+        } else if (floor == nextStop) {
+            state = ElevatorState.SERVING;
+        } else if (floor < nextStop) {
+            state = ElevatorState.MOVING_UP;
+        } else {
+            state = ElevatorState.MOVING_DOWN;
+        }
+    }
+
+    private int getDirectionToMove() {
+        switch (state) {
+            case MOVING_UP:
+                return 1;
+            case MOVING_DOWN:
+                return -1;
+            case IDLE:
+            case SERVING:
+            default:
+                return 0;
+        }
+    }
+}
+
+
---
+++ src/com/dos/elevator/ElevatorQueue.java
@@ -0,0 +1,54 @@
+package com.dos.elevator;
+
+import java.util.LinkedList;
+
+public class ElevatorQueue {
+    LinkedList<Integer> queue;
+
+    public ElevatorQueue() {
+        queue = new LinkedList<>();
+}
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public Integer peek() {
+        return queue.peek();
+    }
+
+    public Integer poll() {
+        return queue.poll();
+    }
+
+    public void add(Integer floor) {
+        for (int i = 0; i < queue.size() - 1; i++) {
+            var curr = queue.get(i);
+            if (floor.equals(curr)) {
+                // floor is already in queue
+                return;
+            }
+            if (i == queue.size() - 1) {
+                // if we reach the last value, the only possible place to place the number is at the end
+                queue.add(floor);
+                return;
+            }
+            // check to see if the number can be placed in between two numbers
+            var next = queue.get(i + 1);
+            if (isNumberBetween(floor, curr, next)) {
+                queue.add(i + 1, floor);
+                return;
+            }
+        }
+    }
+
+    public void add(Iterable<Integer> floors) {
+        floors.forEach(this::add);
+    }
+
+    private boolean isNumberBetween(int num, int from, int to) {
+        int max = Integer.max(from, to);
+        int min = Integer.min(from, to);
+        return num > min && num < max;
+    }
+}
